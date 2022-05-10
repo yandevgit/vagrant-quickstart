600001e75b0342a3f33ff56ab4cf22a0fef81e73
yanever <lovenkendemyannick@gmail.com>
2022-05-10 06:44:27 +0100

    update for Timcat 8 Bash shell provisioning

--- provision-tomcat.sh
+++ provision-tomcat.sh
@@ -3,3 +3,26 @@
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y default-jdk
+
+useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+
+apt-get install -y unzip wget
+
+cd /tmp
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.78/bin/apache-tomcat-8.5.78.tar.gz
+
+tar -xzvf apache-tomcat-8.5.78.tar.gz
+mkdir -p /opt/tomcat
+mv apache-tomcat-8.5.78 /opt/tomcat/
+
+ln -s /opt/tomcat/apache-tomcat-8.5.78 /opt/tomcat/latest
+
+chown -R tomcat: /opt/tomcat
+
+sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
+
+cp /vagrant/tomcat.service /etc/init.d/tomcat.service
+chmod 755 /etc/init.d/tomcat.service
+update-rc.d tomcat.service defaults
+
+service tomcat start
